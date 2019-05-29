57f7b21f448ab0fb1d13727cf2ba40ee4cf5705f
P!eum <test.terrific@gmail.com>
2019-05-29 20:59:51 +0700

    Index and show for venue artist promotion

--- routes/user/promotion.js
+++ routes/user/promotion.js
@@ -12,17 +12,22 @@ router.get("/", function (req, res) {
           throw err;
        }
        console.log(promotionInfo)
+       promotionInfo.forEach(function(item){
+         item.Promotion_Start = dateFormat(item.Promotion_Start,"mediumDate");
+         item.Promotion_End = dateFormat(item.Promotion_End,"mediumDate");
+       });
        res.render("./user/promotion/promotionindex", {promotion : promotionInfo});
     });
  });
 
  router.get("/:name", function (req, res) {
     var promotionName = req.params.name.split("-").join(" ");
-    var getpromotionInfo = "SELECT *  FROM promotion WHERE promotion_Name = ?";
+    var getpromotionInfo = "SELECT *  FROM promotion p, membertype mt, member_promotion mp, concert c WHERE p.Promotion_ID = mp.Promotion_ID AND mp.MemberType_ID = mt.MemberType_ID AND p.Concert_ID = c.Concert_ID AND p.Promotion_Name = ? GROUP BY p.Promotion_ID ";
     connection.query(getpromotionInfo,[promotionName], function (err, promotionInfo) {
        if (err) {
           throw err;
        }
+       console.log("ho");
        console.log(promotionInfo[0])
        res.render("./user/promotion/promotionshow", {promotion : promotionInfo[0]});
     });
