d9252d2289ec5ed396d34b88b277b5083cd581a8
P!eum <test.terrific@gmail.com>
2019-05-30 06:58:25 +0700

    Add data analysis

--- routes/user/index.js
+++ routes/user/index.js
@@ -118,7 +118,7 @@ router.get("/logout", function (req, res) {
 
 
 router.get("/statistic", function (req, res) {
-   var table1 = "SELECT * FROM venue ORDER BY Venue_SeatingCapacity DESC;";
+   var table1 = "SELECT c.Concert_Name,e.Concert_ShowDate,SUM(z.Zone_Capacity) AS Capacity FROM concert c,zone z,concert_showtime e WHERE c.Concert_ID = e.Concert_ID AND e.Concert_ShowTime_ID = z.Concert_ShowTime_ID GROUP BY c.Concert_ID , e.Concert_ShowTime_ID ORDER BY Capacity DESC";
    connection.query(table1, function (err, table1) {
       if (err) {
          throw err;
@@ -138,42 +138,42 @@ router.get("/statistic", function (req, res) {
                if (err) {
                   throw err;
                }
-               var table5 = "SELECT c.Concert_Name, COUNT(ca.Concert_Artist_ID) AS Number FROM concert c, concert_artist ca WHERE c.Concert_ID = ca.Concert_Artist_ID GROUP BY ca.Concert_ID   ORDER BY Number DESC;"
+               var table5 = "SELECT c.Concert_Name,MAX(z.Zone_Price) AS MostExpensive FROM concert c,artist a,concert_artist d,zone z,concert_showtime e WHERE c.Concert_ID = e.Concert_ID AND e.Concert_ShowTime_ID = z.Concert_ShowTime_ID AND c.Concert_ID = d.Concert_ID AND d.Artist_ID = a.Artist_ID GROUP BY c.Concert_ID ORDER BY MostExpensive DESC"
                connection.query(table5, function (err, table5) {
                   if (err) {
                      throw err;
                   }
-                  var table6 = "SELECT c.Concert_Name, COUNT(b.Booking_ID) AS Number_of_Booking FROM concert c, booking b, concert_showtime cs WHERE b.Concert_ShowTime_ID = cs.Concert_ShowTime_ID AND cs.Concert_ID = c.Concert_ID GROUP BY cs.Concert_ShowTime_ID ORDER BY Number_of_Booking DESC;"
+                  var table6 = "SELECT r.Record_Label_Name,COUNT(a.Artist_ID) AS NumberofArtist FROM record_label r,artist a WHERE r.Record_Label_ID = a.Record_Label_ID GROUP BY a.Record_Label_ID ORDER BY NumberofArtist DESC"                  
                   connection.query(table6, function (err, table6) {
                      if (err) {
                         throw err;
                      }
-                     var table7 = "SELECT";
+                     var table7 = "SELECT c.Concert_Name,COUNT(a.Artist_ID) AS NumberofArtist FROM concert c,concert_artist a WHERE a.Concert_ID=c.Concert_ID GROUP BY a.Concert_ID ORDER BY NumberofArtist DESC";
                      connection.query(table7, function (err, table7) {
                         if (err) {
                            throw err;
                         }
-                        var table8 = "SELECT c.Concert_Name, COUNT(b.Booking_ID) AS Number_of_Booking FROM concert c, booking b, concert_showtime cs WHERE b.Concert_ShowTime_ID = cs.Concert_ShowTime_ID AND cs.Concert_ID = c.Concert_ID GROUP BY cs.Concert_ShowTime_ID ORDER BY Number_of_Booking DESC;"
+                        var table8 = "SELECT m.Member_Name,d.Concert_Name,SUM(b.Booking_Total_Price) AS Price FROM concert_showtime c,booking b,member m,concert d WHERE c.Concert_ID = d.Concert_ID AND c.Concert_Showtime_ID = b.Concert_Showtime_ID AND m.Member_Username = b.Member_Username GROUP BY b.Member_Username, d.Concert_ID ORDER BY Price DESC;"
                         connection.query(table8, function (err, table8) {
                            if (err) {
                               throw err;
                            }
-                           var table9 = "SELECT c.Concert_Name, COUNT(b.Booking_ID) AS Number_of_Booking FROM concert c, booking b, concert_showtime cs WHERE b.Concert_ShowTime_ID = cs.Concert_ShowTime_ID AND cs.Concert_ID = c.Concert_ID GROUP BY cs.Concert_ShowTime_ID ORDER BY Number_of_Booking DESC;"
+                           var table9 = "SELECT t.Ticket_Receiving_Name,COUNT(b.Ticket_Receiving_ID) AS NumberofUsingMethod FROM booking b,ticket_receiving t WHERE t.Ticket_Receiving_ID = b.Ticket_Receiving_ID GROUP BY t.Ticket_Receiving_ID ORDER BY NumberofUsingMethod DESC"                           
                            connection.query(table9, function (err, table9) {
                               if (err) {
                                  throw err;
                               }
-                              var table10 = "SELECT";
+                              var table10 = "SELECT a.Artist_Name,COUNT(c.Concert_ID) AS NumberofConcert FROM artist a,concert_artist c WHERE a.Artist_ID = c.Artist_ID GROUP BY a.Artist_ID ORDER BY NumberofConcert DESC";
                               connection.query(table10, function (err, table10) {
                                  if (err) {
                                     throw err;
                                  }
-                                 var table11 = "SELECT c.Concert_Name, COUNT(b.Booking_ID) AS Number_of_Booking FROM concert c, booking b, concert_showtime cs WHERE b.Concert_ShowTime_ID = cs.Concert_ShowTime_ID AND cs.Concert_ID = c.Concert_ID GROUP BY cs.Concert_ShowTime_ID ORDER BY Number_of_Booking DESC;"
+                                 var table11 = "SELECT m.Member_Name,COUNT(b.Booking_ID) AS NumberofBooking FROM member m,booking b WHERE m.Member_Username = b.Member_Username GROUP BY b.Member_Username ORDER BY NumberofBooking DESC"
                                  connection.query(table11, function (err, table11) {
                                     if (err) {
                                        throw err;
                                     }
-                                    var table12 = "SELECT c.Concert_Name, COUNT(b.Booking_ID) AS Number_of_Booking FROM concert c, booking b, concert_showtime cs WHERE b.Concert_ShowTime_ID = cs.Concert_ShowTime_ID AND cs.Concert_ID = c.Concert_ID GROUP BY cs.Concert_ShowTime_ID ORDER BY Number_of_Booking DESC;"
+                                    var table12 = "SELECT v.Venue_Name,COUNT(c.Concert_ID) AS NumberofConcert FROM concert c,venue v WHERE v.Venue_ID = c.Venue_ID GROUP BY c.Venue_ID ORDER BY NumberofConcert DESC"
                                     connection.query(table12, function (err, table12) {
                                        if (err) {
                                           throw err;
@@ -182,15 +182,15 @@ router.get("/statistic", function (req, res) {
                                           table1 : table1,
                                           table2 : table2,
                                           table3 : table3,
-                                          talbe4 : table4,
+                                          table4 : table4,
                                           table5 : table5,
                                           table6 : table6,
                                           table7 : table7,
-                                          talbe8 : table8,
+                                          table8 : table8,
                                           table9 : table9,
                                           table10 : table10,
                                           table11 : table11,
-                                          talbe12 : table12
+                                          table12 : table12
                                        })
                                     });
                                  });
